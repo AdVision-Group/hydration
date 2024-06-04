3e97f0ab3b6920ef1c92fbb2caa5dfa2e1ea871e
Samuel Wittlinger <samo.wittlinger@gmail.com>
2024-06-04 12:30:32 +0200

    fix: bullet animation

--- src/components/sections/empowering-daos/desktop/bullet.tsx
+++ src/components/sections/empowering-daos/desktop/bullet.tsx
@@ -1,4 +1,6 @@
 import { twMerge } from "tailwind-merge";
+import { motion } from "framer-motion";
+import { fadeUp } from "@/animation/variants";
 
 type BulletProps = {
   active: boolean;
@@ -6,7 +8,8 @@ type BulletProps = {
 
 export default function Bullet({ active }: BulletProps) {
   return (
-    <div
+    <motion.div
+      variants={fadeUp()}
       className={twMerge(
         "flex transition rounded-full w-3 h-3",
         active ? "bg-pink" : "bg-[#816093]"
