9868c3324000f815955a96abc9e0c42fc77c1456
Samuel Wittlinger <samo.wittlinger@gmail.com>
2024-06-05 15:45:36 +0200

    fix: empowering daos mobile animation

--- src/components/sections/empowering-daos/mobile/content.tsx
+++ src/components/sections/empowering-daos/mobile/content.tsx
@@ -1,15 +1,23 @@
+"use client";
+
 import SectionLabel from "@/components/ui/labels/section";
 import Heading from "@/components/ui/typography/heading";
 import { bulletPoints } from "../data";
 import Paragraph from "@/components/ui/typography/paragraph";
+import AnimateOnView from "@/animation/motion-section";
+import { fadeIn } from "@/animation/variants";
 
 export default function EmpoweringDaosMobileContent() {
   return (
-    <section className="bg-purple ~py-12/32">
+    <AnimateOnView className="bg-purple ~py-12/32">
       <div className="flex flex-col ~gap-12/40 container mx-auto">
         <div className="flex flex-col gap-4">
           <SectionLabel>Empowering DAOs</SectionLabel>
-          <Heading size="medium" className="text-lavender">
+          <Heading
+            size="medium"
+            className="text-lavender"
+            animationVariants={fadeIn()}
+          >
             15+ projects and DAOs manage their liquidity using Hydration,
             including the Treasury of Polkadot.
           </Heading>
@@ -27,6 +35,6 @@ export default function EmpoweringDaosMobileContent() {
           ))}
         </div>
       </div>
-    </section>
+    </AnimateOnView>
   );
 }
