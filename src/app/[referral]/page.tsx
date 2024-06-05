16d04b4c5bd57fa494f6248cb81e3b127884e502
Samuel Wittlinger <samo.wittlinger@gmail.com>
2024-06-05 18:01:59 +0200

    feat: opengraph image ref

--- src/app/[referral]/page.tsx
+++ src/app/[referral]/page.tsx
@@ -1,4 +1,33 @@
+import { Metadata } from "next";
 import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://hydration.net"),
+  openGraph: {
+    images: [
+      {
+        url: "https://hydration.net/opengraph-image-ref.png",
+        protocol: "https",
+        hostname: "hydration.net",
+        width: 1200,
+        height: 627,
+        alt: "Hydration | Finance made efficient",
+      },
+    ],
+  },
+  twitter: {
+    images: [
+      {
+        url: "https://hydration.net/twitter-image-ref.png",
+        protocol: "https",
+        hostname: "hydration.net",
+        width: 1200,
+        height: 627,
+        alt: "Hydration | Finance made efficient",
+      },
+    ],
+  },
+};
 
 export default function ReferralPage({
   params: { referral },
