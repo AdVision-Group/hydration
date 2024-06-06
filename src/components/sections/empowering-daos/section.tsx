d771b2a036596cce7cae113b65b11e0fdbfb40d4
Samuel Wittlinger <samo.wittlinger@gmail.com>
2024-06-06 09:39:13 +0200

    fix: missing usecallback in empowering daos

--- src/components/sections/empowering-daos/section.tsx
+++ src/components/sections/empowering-daos/section.tsx
@@ -3,13 +3,17 @@
 import useScreenSize from "@/hooks/useScreenSize";
 import EmpoweringDaosDesktopContent from "./desktop/content";
 import EmpoweringDaosMobileContent from "./mobile/content";
+import { useMemo } from "react";
 
 export default function EmpoweringDaosSection() {
   // Using js to determine if the screen is mobile to avoid unnecessary scroll lock calculations
   // on mobile devices
 
   const { width, height } = useScreenSize();
-  const showMobile = width < 1024 || height < 850;
+  const showMobile = useMemo(
+    () => width < 1024 || height < 850,
+    [width, height]
+  );
 
   return showMobile ? (
     <EmpoweringDaosMobileContent />
